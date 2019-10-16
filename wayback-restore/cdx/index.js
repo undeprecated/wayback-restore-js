628c45e5e2a7bbb4a8fc468c721d2026ca45be04
Nicholas Curry <nick@Nicholass-MBP.fios-router.home>
2019-10-16 18:02:58 -0400

    changed package name to wayback-restore

--- wayback-restore/cdx/index.js
+++ wayback-restore/cdx/index.js
@@ -1,20 +1,18 @@
-/*jshint node: true*/
+/* jshint node: true */
 /*global require, module */
 
 /**
  * Utility for querying the Wayback Machine CDX server.
  */
 
-'use strict';
-var _ = require('lodash'),
-    //querystring = require('querystring'),
-    request = require('request');
+"use strict";
+var request = require("request");
 
-var ArrayTransform = require('./array-transform'),
-    JsonTransform = require('./json-transform'),
-    FIELDS = require('./fields');
+var ArrayTransform = require("./array-transform"),
+    JsonTransform = require("./json-transform"),
+    FIELDS = require("./fields");
 
-var CDX_SERVER = 'https://web.archive.org/cdx/search/cdx';
+var CDX_SERVER = "https://web.archive.org/cdx/search/cdx";
 
 module.exports = {
     /**
@@ -23,31 +21,32 @@ module.exports = {
      * @return JSON string
      */
     stream: function(options) {
-        options = _.merge({
-            url: '',
-            fl: [
-                FIELDS.URLKEY,
-                FIELDS.TIMESTAMP,
-                FIELDS.ORIGINAL,
-                FIELDS.MIMETYPE,
-                FIELDS.STATUSCODE,
-                FIELDS.DIGEST,
-                FIELDS.LENGTH
-            ],
-            outputFormat: 'json',
-            matchType: null,
-            // @TODO - need to un-gzip output
-            //gzip: 'true',
+        options = Object.assign(
+            {
+                url: "",
+                fl: [
+                    FIELDS.URLKEY,
+                    FIELDS.TIMESTAMP,
+                    FIELDS.ORIGINAL,
+                    FIELDS.MIMETYPE,
+                    FIELDS.STATUSCODE,
+                    FIELDS.DIGEST,
+                    FIELDS.LENGTH
+                ],
+                outputFormat: "json",
+                matchType: null,
+                // @TODO - need to un-gzip output
+                //gzip: 'true',
 
-            filter: null,
-            limit: null,
-            offset: null,
-            to: null
-        }, options);
+                filter: null,
+                limit: null,
+                offset: null,
+                to: null
+            },
+            options
+        );
 
-        var arrayTransform = new ArrayTransform({
-            objectMode: true
-        });
+        var arrayTransform = new ArrayTransform({ objectMode: true });
 
         var jsonTransform = new JsonTransform({
             objectMode: true,
@@ -60,18 +59,17 @@ module.exports = {
             if (options.hasOwnProperty(key) && options[key] !== null) {
                 var value;
 
-                if (key === 'fl') {
-                    value = options[key].join(',');
+                if (key === "fl") {
+                    value = options[key].join(",");
                 } else {
                     value = options[key];
                 }
 
-                querystring.push(key + '=' + value);
+                querystring.push(key + "=" + value);
             }
         }
 
-        var url = CDX_SERVER +
-            '?' + querystring.join('&');
+        var url = CDX_SERVER + "?" + querystring.join("&");
 
         return request(url)
             .pipe(arrayTransform)
