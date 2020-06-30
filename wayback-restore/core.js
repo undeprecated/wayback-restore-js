c8f803f382890e2a2e483f935d502813ba39fbf1
Nick Curry <cnick79@gmail.com>
2020-06-30 15:24:30 -0400

    added process stop

--- wayback-restore/core.js
+++ wayback-restore/core.js
@@ -1,19 +1,20 @@
 module.exports = {
-    VERSION: '1.0.0',
-    ARCHIVE_SOURCE: 'http://web.archive.org',
-    ARCHIVE_TEMPLATE: 'https://web.archive.org/web/',
+    VERSION: "1.0.0",
+    ARCHIVE_SOURCE: "http://web.archive.org",
+    ARCHIVE_TEMPLATE: "https://web.archive.org/web/",
     RESTORE_STATUS: {
-        RESTORED: 'restored',
-        FAILED: 'failed',
-        UNARCHIVED: 'unarchived',
-        RESTORING: 'restoring',
-        EMPTY: 'notrestored'
+        RESTORED: "restored",
+        FAILED: "failed",
+        UNARCHIVED: "unarchived",
+        RESTORING: "restoring",
+        EMPTY: "notrestored"
     },
     EVENTS: {
-        STARTED: 'start',
-        RESTORING: 'restoring',
-        RESTORED: 'restored',
-        COMPLETED: 'completed',
-        FAILED: 'failed'
+        STARTED: "start",
+        STOP: "stop",
+        RESTORING: "restoring",
+        RESTORED: "restored",
+        COMPLETED: "completed",
+        FAILED: "failed"
     }
 };
