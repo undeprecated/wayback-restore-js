fea0c0d7399abd0a35c770d2d4ac4804b2c7fca1
nick <nick@undeprecated.com>
2021-07-09 15:05:26 -0400

    0.0.14

--- wayback-restore/core.js
+++ wayback-restore/core.js
@@ -1,26 +1,26 @@
 module.exports = {
-  VERSION: "0.0.2",
-  ARCHIVE_SOURCE: "http://web.archive.org",
-  ARCHIVE_TEMPLATE: "https://web.archive.org/web/",
-  ARCHIVE_SOURCE_RE: new RegExp("http://web.archive.org", "i"),
-  ARCHIVE_TEMPLATE_RE: new RegExp("https://web.archive.org/web/", "i"),
+  VERSION: '0.0.14',
+  ARCHIVE_SOURCE: 'http://web.archive.org',
+  ARCHIVE_TEMPLATE: 'https://web.archive.org/web/',
+  ARCHIVE_SOURCE_RE: new RegExp('http://web.archive.org', 'i'),
+  ARCHIVE_TEMPLATE_RE: new RegExp('https://web.archive.org/web/', 'i'),
   RESTORE_STATUS: {
-    RESTORED: "restored",
-    FAILED: "failed",
-    UNARCHIVED: "unarchived",
-    RESTORING: "restoring",
-    EMPTY: ""
+    RESTORED: 'restored',
+    FAILED: 'failed',
+    UNARCHIVED: 'unarchived',
+    RESTORING: 'restoring',
+    EMPTY: ''
   },
   EVENTS: {
-    PAUSED: "paused",
-    RESUMED: "resumed",
-    STARTED: "start",
-    STOP: "stop",
-    RESTORING: "restoring",
-    RESTORED: "restored",
-    COMPLETED: "completed",
-    FAILED: "failed",
-    CDXQUERY: "cdxquery",
-    ERROR: "error"
+    PAUSED: 'paused',
+    RESUMED: 'resumed',
+    STARTED: 'start',
+    STOP: 'stop',
+    RESTORING: 'restoring',
+    RESTORED: 'restored',
+    COMPLETED: 'completed',
+    FAILED: 'failed',
+    CDXQUERY: 'cdxquery',
+    ERROR: 'error'
   }
 };
